0ff2925dc67cecd9080a93e77796320060d95049
izabelaellys <iza.ellys@gmail.com>
2023-06-14 01:39:46 -0300

    set menus
    add menu normal ou admin a home

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -1,6 +1,7 @@
 const sectionLogado = document.querySelector('.logadosection')
 const sectionLogar = document.querySelector('.loginsection')
 const menuComum = document.querySelector('.menu')
+const menuAdmin = document.querySelector('.menu-admin')
 const userName = document.querySelector('#username')
 const dataName = localStorage.getItem('name')
 const logoutButton = document.querySelector('#logout')
@@ -9,14 +10,18 @@ const logoutButton = document.querySelector('#logout')
 if(dataName){
   sectionLogado.classList.add('active')
   sectionLogar.classList.remove('active')
-  menuComum.classList.add('active')
   userName.innerHTML += dataName
+  if(localStorage.getItem('roles') == 'ROLE_ADMIN'){
+    menuAdmin.classList.add('active')
+  }else{
+    menuComum.classList.add('active')
+  }
 }else{
   sectionLogado.classList.remove('active')
   sectionLogar.classList.add('active')
   menuComum.classList.remove('active')
+  menuAdmin.classList.remove('active')
 }
-
 
 // Realiza o logout
 logoutButton.addEventListener('click', () => {
