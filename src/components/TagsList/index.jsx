a17220a8b115e81ef6f46fb05cd61cea910db9a9
izabelaellys <iza.ellys@gmail.com>
2023-06-26 14:32:56 -0300

    finalizacao pg de tags

--- src/components/TagsList/index.jsx
+++ src/components/TagsList/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { StyledTagsList, TagCard } from "./styles";
+import { StyledTagsList, TagCard, TagsInfo } from "./styles";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Button from "../Button";
@@ -13,6 +13,8 @@ const TagsList = () => {
   const [tags, setTags] = useState()
   const [totalPages, setTotalPages] = useState()
   const [MyCookie, setMyCookie] = useState();
+  const [tagsSelecionadas, setTagsSelecionadas] = useState();
+  const [limiteTag, setLimiteTag ] = useState(0)
 
   useEffect(() => {
     setMyCookie(cookie?.get("myCookie"));
@@ -24,7 +26,7 @@ const TagsList = () => {
       const url = "http://localhost:8080/api/tag/v1.1/paginated-desc/" + (page || 1) + "/" + itemsByPage
       try {
         const response = await axios.get(url)
-        console.log(response.data)
+        
         setTags(response?.data?.tagDtos)
         setTotalPages(response?.data?.maxResults)
       } catch (error) {
@@ -53,19 +55,61 @@ const TagsList = () => {
     
   }, [page, search])
 
+
+  useEffect(() => {
+    if(!tagsSelecionadas && cookie.get('tags')){
+      setTagsSelecionadas(cookie.get('tags').split(','))
+    }
+  }, [tagsSelecionadas])
+
+
+  console.log(tagsSelecionadas)
+
+  const geraLink = () => {
+    const tags = tagsSelecionadas?.join('+')
+    const url = "/post-by-tag?page=1&tag=" + tags
+
+    return url
+  }
+
+  const toogleTag = (tag) => {
+    if(tagsSelecionadas.length < 5){
+      setLimiteTag(0)
+      if(tagsSelecionadas && !tagsSelecionadas.includes(tag) && tagsSelecionadas != undefined){
+        const newTags = [...tagsSelecionadas, tag];
+        setTagsSelecionadas(newTags)
+      }else if(!tagsSelecionadas?.includes(tag)){
+        setTagsSelecionadas([tag.toString()])
+      }
+    }else{
+      setLimiteTag(1)
+    }
+    
+    if(tagsSelecionadas?.includes(tag)){
+      setLimiteTag(0)
+
+      const newTags = tagsSelecionadas.filter((item) => item !== tag);
+      setTagsSelecionadas(newTags)
+    }
+
+    cookie.set('tags', tagsSelecionadas)
+  }
+
   return (
     <StyledTagsList>
       <div class="taghead">
         <p>Nome da TAG</p>
+        <p>Quantidade de posts</p>
         <p>Visualizar filtro</p>
       </div>
 
       <div class="tagcontent">
         {tags?.map((item) => {
           return (
-            <TagCard>
-              <p>{item.nome}</p>
-              <div class="viewcontainer">
+            <TagCard className={tagsSelecionadas.includes(item.nome) ? 'active' : ''}>
+              <p className="tagName">{item?.nome}</p>
+              <p className="tagQt">{item?.qtdePosts}</p>
+              <div class="viewcontainer" onClick={() => toogleTag(item?.nome)} >
                 <img src="view.png" alt="" />
               </div>
             </TagCard>
@@ -78,6 +122,20 @@ const TagsList = () => {
         {!search && !page && <Button link={`/tags?page=2`} title="Ver mais" />}
         {search && <Button link="/tags?page=1" title="Voltar" />}
       </div>
+      <TagsInfo>
+        <div className="tag-info-card">
+          <div className="tag-info-card-head">Tags fixadas</div>
+          <div className="tag-info-card-body">
+            <p className={limiteTag == 1 ? 'alert active' : 'alert'}>Só é possível fixar até 5 tags</p>
+            <div className="tags-selecionadas-container">
+              {tagsSelecionadas?.map(botao => {
+                return <button className="tagbutton">{botao}</button>
+              })}
+            </div>
+            <Button title="Pesquisar posts" link={geraLink()} />
+          </div>
+        </div>
+      </TagsInfo>
     </StyledTagsList>
   );
 };
--- src/components/TagsList/styles.jsx
+++ src/components/TagsList/styles.jsx
@@ -12,12 +12,16 @@ export const StyledTagsList = styled.div`
       font-size: 20px;
       line-height: 27px;
     }
+
+    p:first-of-type {
+      width: 240px;
+    }
   }
 
-  .paginationcontainer{
+  .paginationcontainer {
     margin-top: 20px;
-    
-    .btn{
+
+    .btn {
       margin: 0 auto;
       width: 200px;
     }
@@ -32,6 +36,15 @@ export const TagCard = styled.div`
   height: 64px;
   cursor: pointer;
 
+  .tagName {
+    width: 240px;
+  }
+
+  .tagQt {
+    text-align: left;
+    width: 100px;
+  }
+
   &.active .viewcontainer {
     background-color: #26b11a;
   }
@@ -44,5 +57,92 @@ export const TagCard = styled.div`
     height: 39px;
     background-color: #eb3d3d;
     border-radius: 15px;
+  }
+`;
+
+export const TagsInfo = styled.div`
+  position: fixed;
+  top: 120px;
+  right: 0px;
+  width: 356px;
+  overflow-y: scroll;
+
+  .tag-info-card {
+    width: 260px;
+    margin: 0 0 0 56px;
+
+    &:last-of-type {
+      margin-top: 56px;
+    }
+
+    .tag-info-card-head {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 49px;
+      background: #efefef;
+      border: 1px solid #efefef;
+      backdrop-filter: blur(2px);
+      border-radius: 5px 5px 0px 0px;
+
+      font-weight: bold;
+      font-size: 15px;
+      line-height: 20px;
+      color: #000000;
+    }
+
+    .tag-info-card-body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background: rgba(217, 217, 217, 0.12);
+      border: 1px solid #efefef;
+      backdrop-filter: blur(2px);
+      border-radius: 0px 0px 5px 5px;
+      padding: 43px 16px 16px;
+
+      .alert {
+        display: none;
+        font-weight: bold;
+        font-size: 15px;
+        line-height: 20px;
+        color: #000000;
+        margin: 10px 0;
+
+        &.active {
+          display: block;
+        }
+      }
+
+      .btn {
+        margin-top: 20px;
+      }
+
+      .tags-selecionadas-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        row-gap: 10px;
+        column-gap: 10px;
+
+        button {
+          display: flex;
+          align-items: center;
+          flex-wrap: nowrap;
+          background-color: transparent;
+          border-radius: 5px;
+          border: 2px solid #26b11a;
+          font-weight: bold;
+          font-size: 15px;
+          line-height: 20px;
+          transition: all 0.2s ease-in-out;
+          height: 30px;
+
+          ${'' /* &:hover {
+            border: 2px solid #eb3d3d;
+          } */}
+        }
+      }
+    }
   }
 `;
